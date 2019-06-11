ae00497fa0026845da3ee4c648d91666ee328890
Eco01 <46776444+Eco01@users.noreply.github.com>
2019-06-11 19:34:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const myid = [''];
-const prefix = ['..'];
+const prefix = ['1'];
 
 client.on('ready', () => {
    console.log(`----------------`);

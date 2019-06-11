1933de0711eb099d655d50891797d96189078834
Eco01 <46776444+Eco01@users.noreply.github.com>
2019-06-11 18:11:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const myid = [''];
-const prefix = ['.'];
+const prefix = ['..'];
 
 client.on('ready', () => {
    console.log(`----------------`);
@@ -31,7 +31,7 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if(message.content === prefix+'creadits'){
+    if(message.content === prefix+'credits'){
         message.channel.send('#credits')
     }
 });
@@ -47,7 +47,7 @@ if (message.content === prefix+'spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`** Hi !!**[ " ${x} " ]`)
+        message.channel.send(`** يا جامي **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
@@ -82,7 +82,7 @@ client2.on('message', message => {
 });
 
 client2.on('message', message => {
-    if(message.content === prefix+'creadits'){
+    if(message.content === prefix+'credits'){
         message.channel.send('#credits')
     }
 });
@@ -98,7 +98,7 @@ if (message.content === prefix+'spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`** bye !!**[ " ${x} " ]`)
+        message.channel.send(`** يا ابو الصحاب **[ " ${x} " ]`)
           .then(m => {
             count++;
           })

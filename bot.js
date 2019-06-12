5d16efad88e6e10096af496ff43617bc65333a75
Eco01 <46776444+Eco01@users.noreply.github.com>
2019-06-12 16:42:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -152,7 +152,7 @@ if (message.content === prefix+'spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`** يا يوسف  **[ " ${x} " ]`)
+        message.channel.send(`**Hey Yousef **[ " ${x} " ]`)
           .then(m => {
             count++;
           })

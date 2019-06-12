4c746d4b9be99a70695f82ad0eebc689004fb374
Eco01 <46776444+Eco01@users.noreply.github.com>
2019-06-12 15:57:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -128,6 +128,21 @@ if (!rank) return message.reply(' ')
 
 
 client.login(process.env.TOKEN);
+//say
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "2say") {
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
 client2.login(process.env.TOKEN2);
 
 //say

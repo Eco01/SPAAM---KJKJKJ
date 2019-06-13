2b9c9caddac2c602b416516c9e0141c985b9282d
Eco01 <46776444+Eco01@users.noreply.github.com>
2019-06-13 14:12:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,6 +25,71 @@ client2.on('ready', () => {
 
 
 //bot 1
+client.on('message', message => { 
+    
+    function rasteleSembol(uzunluk, semboller) {
+
+var maske = '';
+
+if (semboller.indexOf('a') > -1) maske += 'abcdefghijklmnopqrstuvwxyz';
+
+if (semboller.indexOf('A') > -1) maske += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+if (semboller.indexOf('0') > -1) maske += '0123456789';
+
+
+var sonuc = '';
+
+ 
+
+for (var i = uzunluk; i > 0; --i) 
+
+{
+
+sonuc += maske[Math.floor(Math.random() * maske.length)];
+
+}
+
+return sonuc;
+
+}
+
+function spamla(){
+    message.channel.send("https:/"+"/discord.gift/" + rasteleSembol(16,'0aA'));
+}
+
+    if (message.channel.id = "588699204507926539"){
+        if(message.content.startsWith("hrr!nitro ")){
+        try{
+        var veri = message.content.toString();
+        var veriSn = veri.substring(10)
+         var saniye = parseInt(veriSn);
+         var saniye1 = saniye * 1000;
+        if (saniye > 4){
+        setInterval(spamla, saniye1);
+        }
+        
+        else{
+            message.reply("krdş ya saniye kısmı 5 saniyeden az ya da saniye kısmına başka bişiy yazıyon, senle mi uğraşçam?");
+        }
+        }
+        catch(err){
+            message.channel.send("Teknik bir hata oluştu!");
+        }
+        }
+        }
+     
+
+
+}); 
+
+
+
+
+
+
+
+//spam code 1
 client.on('message', message => {
     if(message.content === prefix+'daily'){
         message.channel.send('#daily')

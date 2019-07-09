a9e4c4f3a86a478fb350d46604a00abbe8933a4f
Eco01 <46776444+Eco01@users.noreply.github.com>
2019-07-09 16:31:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -97,8 +97,8 @@ client2.on('message', message => {
 if (message.content === prefix+'spam') {
       let count = 0;
       let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+      for(let x = 0; x < 900000; x++) {
+        message.channel.send(`**I have discord **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
